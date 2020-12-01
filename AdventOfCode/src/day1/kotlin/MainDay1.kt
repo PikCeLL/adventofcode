3316b2d6eb26326b54b57933449a59d03599a303
PikCeLL <v.levallois@laposte.net>
2020-12-01 22:36:40 +0100

    Day1

---
+++ AdventOfCode/src/day1/kotlin/MainDay1.kt
@@ -0,0 +1,26 @@
+package day1
+
+import kotlin.math.exp
+
+fun main() {
+    print("Target?")
+    val sum: Int = Integer.decode(readLine())
+    print(result(sum))
+}
+
+fun result(sum: Int): Int {
+    val result: Int
+    val fileContent: String = {}::class.java.getResource("/day1.txt").readText()
+    val expenses: List<Int> = fileContent.lineSequence().map { line -> Integer.decode(line) }.toList()
+
+    expenses.forEach {
+        expense -> expenses.forEach {
+            expense2 -> expenses.forEach {
+                when(expense + expense2 + it) {
+                    sum -> return expense * expense2 * it
+                }
+            }
+        }
+    }
+    return -1
+}
