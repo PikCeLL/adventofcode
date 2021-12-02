bc2a6d0e519e3d95e3ea5f8b31318a90361ac234
PikCeLL <v.levallois@laposte.net>
2021-12-02 19:17:34 +0100

    2021 : Day2 = when

---
+++ AdventOfCode2021/src/day2/Day2.kt
@@ -0,0 +1,42 @@
+package day2
+
+import utils.IDailyPuzzle
+
+fun main() {
+    Day2().printResults()
+}
+
+class Day2 : IDailyPuzzle {
+    override fun getInputFile(): String {
+        return "/day2.txt"
+    }
+
+    override fun getResultPuzzle1(input: String): Int {
+        return input.lineSequence().fold(Pair(0, 0)) { coord, line ->
+            val displacementValue = line.split(" ")[1].toInt()
+            when {
+                line.startsWith("forward") -> Pair(coord.first + displacementValue, coord.second)
+                line.startsWith("down") -> Pair(coord.first, coord.second + displacementValue)
+                line.startsWith("up") -> Pair(coord.first, coord.second - displacementValue)
+                else -> coord
+            }
+        }.let { it.first * it.second }
+    }
+
+    override fun getResultPuzzle2(input: String): Int {
+        return input.lineSequence().fold(Triple(0, 0, 0)) { coord, line ->
+            val displacementValue = line.split(" ")[1].toInt()
+            when {
+                line.startsWith("forward") -> Triple(
+                    coord.first + displacementValue,
+                    coord.second + (coord.third * displacementValue),
+                    coord.third
+                )
+                line.startsWith("down") -> Triple(coord.first, coord.second, coord.third + displacementValue)
+                line.startsWith("up") -> Triple(coord.first, coord.second, coord.third - displacementValue)
+                else -> coord
+            }
+        }.let { it.first * it.second }
+    }
+}
+
---
+++ AdventOfCode2021/tests/day2/TestDay2.kt
@@ -0,0 +1,30 @@
+package day2
+
+import ADailyTest
+
+class TestDay2 : ADailyTest<Day2>() {
+    override fun createInstance(): Day2 {
+        return Day2()
+    }
+
+    override fun getInputPuzzle1(): String {
+        return "forward 5\n" +
+                "down 5\n" +
+                "forward 8\n" +
+                "up 3\n" +
+                "down 8\n" +
+                "forward 2"
+    }
+
+    override fun getResultPuzzle1(): Int {
+        return 150
+    }
+
+    override fun getInputPuzzle2(): String {
+        return getInputPuzzle1()
+    }
+
+    override fun getResultPuzzle2(): Int {
+        return 900
+    }
+}
