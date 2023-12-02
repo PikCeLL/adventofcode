3b99584b7ec3d3e1919258a2c3e4a73a8eb0aa01
PikCeLL <v.levallois@laposte.net>
2023-12-02 21:24:49 +0100

    2023 : Day 2 - Very ugly input parsing

---
+++ AdventOfCode2023/src/Day2.kt
@@ -0,0 +1,49 @@
+package aoc2023
+
+import framework.src.IDailyPuzzle
+import kotlin.math.max
+
+fun main() {
+    Day2().printResults()
+}
+
+data class Drawing(val red: Int = 0, val green: Int = 0, val blue: Int = 0)
+data class Game(val id: Int, val drawings: Collection<Drawing>)
+
+class Day2 : IDailyPuzzle {
+    override fun getInputFile(): String {
+        return "/day2.txt"
+    }
+
+    private fun getGameSequence(input: String) = input.lineSequence().map { gameLine ->
+        val splitLine = gameLine.split(':')
+        Game(
+            splitLine[0].substring(4).trim().toInt(),
+            splitLine[1].split(';')
+                .map {
+                    val colors = it.split(',')
+                    Drawing(
+                        red = colors.filter { c -> c.endsWith(" red") }
+                            .map { red -> red.replace(" red", "").trim().toInt() }.getOrElse(0) { 0 },
+                        green = colors.filter { c -> c.endsWith(" green") }
+                            .map { red -> red.replace(" green", "").trim().toInt() }.getOrElse(0) { 0 },
+                        blue = colors.filter { c -> c.endsWith(" blue") }
+                            .map { red -> red.replace(" blue", "").trim().toInt() }.getOrElse(0) { 0 })
+                }
+                .toList())
+    }
+
+    override fun getResultPuzzle1(input: String): Int {
+        return getGameSequence(input).filter { game -> game.drawings.all { it.red <= 12 && it.green <= 13 && it.blue <= 14 } }
+            .sumOf { it.id }
+    }
+
+    override fun getResultPuzzle2(input: String): Int {
+        return getGameSequence(input).map { game ->
+            game.drawings.fold(Triple(0, 0, 0)) { tri, draw ->
+                Triple(max(tri.first, draw.red), max(tri.second, draw.green), max(tri.third, draw.blue))
+            }
+        }.map { it.first * it.second * it.third }.sum()
+    }
+}
+
---
+++ AdventOfCode2023/tests/TestDay2.kt
@@ -0,0 +1,33 @@
+package aoc2023
+
+import framework.tests.ADailyTest
+
+class TestDay2 : ADailyTest<Day2>() {
+    override fun createInstance(): Day2 {
+        return Day2()
+    }
+
+    override fun getInputPuzzle1(): String {
+        return "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    }
+
+    override fun getResultPuzzle1(): Int {
+        return 8
+    }
+
+    override fun getInputPuzzle2(): String {
+        return "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    }
+
+    override fun getResultPuzzle2(): Int {
+        return 2286
+    }
+}
