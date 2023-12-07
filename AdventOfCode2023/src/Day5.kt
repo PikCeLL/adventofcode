b61806672cffcbc3e4cb73855d4997a88886ac73
PikCeLL <v.levallois@laposte.net>
2023-12-07 16:38:55 +0100

    2023 : Day 5 - Part 2 should work, but takes forever

--- AdventOfCode2023/src/Day5.kt
+++ AdventOfCode2023/src/Day5.kt
@@ -1,6 +1,7 @@
 package aoc2023
 
 import framework.src.IDailyPuzzle
+import kotlin.math.min
 
 fun main() {
     Day5().printResults()
@@ -13,7 +14,7 @@ class Day5 : IDailyPuzzle {
 
 
     override fun getResultPuzzle1(input: String): Int {
-        val splitInput = input.split("\\n\\n.*-to-.* map:\\n".toRegex())
+        val splitInput = input.split("\\r?\\n\\r?\\n.*-to-.* map:\\r?\\n".toRegex())
         val initSeeds = splitInput[0].substring("seeds: ".length).split(' ').map { it.toLong() }
         return splitInput.drop(1).fold(initSeeds) { seeds, entry ->
             val ranges = entry.lineSequence().map { map -> map.split(' ').map { it.toLong() } }
@@ -25,18 +26,22 @@ class Day5 : IDailyPuzzle {
     }
 
     override fun getResultPuzzle2(input: String): Int {
-        val splitInput = input.split("\\n\\n.*-to-.* map:\\n".toRegex())
-        val initSeeds = splitInput[0].substring("seeds: ".length)
-            .split(' ')
-            .map { it.toLong() }
-            .windowed(2, 2)
-            .map { LongRange(it[0], it[0] + it[1] - 1) }
-        splitInput.drop(1).fold(initSeeds) { seeds, entry ->
-            val ranges = entry.lineSequence().map { map -> map.split(' ').map { it.toLong() } }
-            seeds.flatMap { listOf(LongRange(it.start, ), LongRange(), LongRange(, it.endInclusive)) }
-            seeds
-        }
-        return 0
+        val splitInput = input.split("\\r?\\n\\r?\\n.*-to-.* map:\\r?\\n".toRegex())
+        val rangesMap = splitInput.drop(1).map { entry -> entry.lineSequence().map { map -> map.split(' ').map { it.toLong() } } }
+        return splitInput[0].substring("seeds: ".length)
+                .split(' ')
+                .map { it.toLong() }
+                .windowed(2, 2)
+                .map {
+                    var rangeMin = Long.MAX_VALUE
+                    for (i in it[0]..<it[0] + it[1]) {
+                        rangeMin = min(rangeMin, rangesMap.fold(i) { seed, entries ->
+                            val range = entries.find { seed in it[1]..<it[1] + it[2] }
+                            if (range != null) seed - range[1] + range[0] else seed
+                        })
+                    }
+                    rangeMin
+                }.min().toInt()
     }
 }
 
