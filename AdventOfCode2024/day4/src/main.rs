220d20c3f18dfce58d36c2856630fccddee83e86
PikCeLL <v.levallois@laposte.net>
2024-12-04 21:49:02 +0100

    2024 - day4 : Matrix traversal

--- AdventOfCode2024/day4/src/main.rs
+++ AdventOfCode2024/day4/src/main.rs
@@ -3,20 +3,20 @@ use std::fs;
 fn main() {
     let p1 = part1();
     println!("The result of the first problem is : {p1}");
-    //let p2 = part2();
-    //println!("The result of the second problem is : {p2}");
+    let p2 = part2();
+    println!("The result of the second problem is : {p2}");
 }
 
 fn count_word_around(matrix: &Vec<Vec<char>>, word: &str, pos: (isize, isize)) -> u32 {
     let mut res = 0;
-    for dir_x in -1..1 {
-        for dir_y in -1..1 {
+    for dir_x in -1..=1 {
+        for dir_y in -1..=1 {
             let mut is_word_complete = true;
             for i in 1..word.len() {
-                let pos_x = pos.0 + dir_x;
-                let pos_y = pos.1 + dir_y;
-                is_word_complete &= (pos_x >= 0) && (pos_y >= 0) && (pos_x <= matrix.len().try_into().unwrap()) && (pos_y <= matrix[0].len().try_into().unwrap())
-                && (word.chars().nth(i).unwrap() == matrix[TryInto::<usize>::try_into(pos_x).unwrap()][TryInto::<usize>::try_into(pos_y).unwrap()]);
+                let pos_x: isize = pos.0 + (i as isize * dir_x);
+                let pos_y: isize = pos.1 + (i as isize * dir_y);
+                is_word_complete &= (pos_x >= 0) && (pos_y >= 0) && (pos_x < matrix.len() as isize) && (pos_y < matrix[0].len() as isize)
+                && (word.chars().nth(i).unwrap() == matrix[pos_x as usize][pos_y as usize]);
             }
             if is_word_complete {
                 res += 1;
@@ -27,7 +27,7 @@ fn count_word_around(matrix: &Vec<Vec<char>>, word: &str, pos: (isize, isize)) -
 }
 
 fn part1() -> u32 {
-    let contents = fs::read_to_string("res/exemple1")
+    let contents = fs::read_to_string("res/input")
         .expect("Should have been able to read the file");
     let matrix = contents.lines().map(|line| line.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
     let word = "XMAS";
@@ -35,7 +35,24 @@ fn part1() -> u32 {
     for (i, line) in matrix.iter().enumerate() {
         for (j, c) in line.iter().enumerate() {
             if word.chars().nth(0).unwrap() == *c {
-                res += count_word_around(&matrix, word, (i.try_into().unwrap(), j.try_into().unwrap()));
+                res += count_word_around(&matrix, word, (i as isize, j as isize));
+            }
+        }
+    }
+    return res;
+}
+
+fn part2() -> u32 {
+    let contents = fs::read_to_string("res/input")
+        .expect("Should have been able to read the file");
+    let matrix = contents.lines().map(|line| line.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let mut res = 0;
+    for i in 1..matrix.len()-1 {
+        for j in 1..matrix[0].len()-1 {
+            if matrix[i][j] == 'A'
+            && ((matrix[i-1][j+1] == 'M' && matrix[i+1][j-1] == 'S') || (matrix[i-1][j+1] == 'S' && matrix[i+1][j-1] == 'M'))
+            && ((matrix[i+1][j+1] == 'M' && matrix[i-1][j-1] == 'S') || (matrix[i+1][j+1] == 'S' && matrix[i-1][j-1] == 'M')) {
+                res += 1;
             }
         }
     }
