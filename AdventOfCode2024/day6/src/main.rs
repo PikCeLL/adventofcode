f7432d518b676aa9a8833c9158b1e96774015aa2
PikCeLL <v.levallois@laposte.net>
2024-12-07 00:25:49 +0100

    2024 - day6 : Part 2, big Map to use the same function for both parts

--- AdventOfCode2024/day6/src/main.rs
+++ AdventOfCode2024/day6/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     let p1 = part1();
@@ -16,7 +16,34 @@ fn next_dir(dir: (isize, isize)) -> (isize, isize) {
         (1,0) => (0,-1),
         (0,-1) => (-1,0),
         _ => (0,0),
-    };
+    }; 
+}
+
+fn guard_patrol(init_guard_pos: (isize, isize), matrix: &Vec<Vec<char>>, extra_block: (isize, isize)) -> (HashMap<(isize, isize), Vec<(isize, isize)>>, bool) {
+    // At first the guard is looking North
+    let mut dir: (isize, isize) = (-1, 0);
+    let mut guard_pos = init_guard_pos;
+    let mut visited_places: HashMap<(isize, isize), Vec<(isize, isize)>> = HashMap::new();
+    let matrix_width = matrix[0].len() as isize;
+    let matrix_height = matrix.len() as isize;
+    while (0..matrix_height).contains(&guard_pos.0) && (0..matrix_width).contains(&guard_pos.1) {
+        if matrix[guard_pos.0 as usize][guard_pos.1 as usize] == '#' || (guard_pos == extra_block) {
+            guard_pos = (guard_pos.0 - dir.0, guard_pos.1 - dir.1);
+            dir = next_dir(dir);
+        } else {
+            if let Some(x) = visited_places.get_mut(&guard_pos) {
+                if x.contains(&dir) {
+                    return (visited_places, true);
+                } else {
+                    x.push(dir);
+                }
+            } else {
+                visited_places.insert(guard_pos, vec![dir]);
+            }
+            guard_pos = (guard_pos.0 + dir.0, guard_pos.1 + dir.1);
+        }
+    }
+    return (visited_places, false);
 }
 
 fn part1() -> u32 {
@@ -33,22 +60,22 @@ fn part1() -> u32 {
             }
         }
     }
-    // At first the guard is looking North
-    let mut dir: (isize, isize) = (-1, 0);
-    let mut visited_places = HashSet::new();
-    while (0..matrix_height).contains(&guard_pos.0) && (0..matrix_width).contains(&guard_pos.1) {
-        if matrix[guard_pos.0 as usize][guard_pos.1 as usize] == '#' {
-            guard_pos = (guard_pos.0 - dir.0, guard_pos.1 - dir.1);
-            dir = next_dir(dir);
-        } else {
-            visited_places.insert(guard_pos);
-            guard_pos = (guard_pos.0 + dir.0, guard_pos.1 + dir.1);
-        }
-    }
-    return visited_places.len() as u32;
+    return guard_patrol(guard_pos, &matrix, (-1, -1)).0.len() as u32;
 }
 
 fn part2() -> u32 {
-    // TODO : garder toutes les positions visitées de la P1, et reboucler dessus en les remplaçant par '#'. Stocker un couple position + direction, et valider la position de l'obstacle si on repasse dans l'une d'elles (pas forcément la première) à un moment
-    return 0;
+    let contents = fs::read_to_string("res/input")
+        .expect("Should have been able to read the file");
+    let matrix = contents.lines().map(|line| line.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let mut guard_pos: (isize, isize) = (0, 0);
+    let matrix_width = matrix[0].len() as isize;
+    let matrix_height = matrix.len() as isize;
+    for i in 0..matrix_height {
+        for j in 0..matrix_width {
+            if matrix[i as usize][j as usize] == '^' {
+                guard_pos = (i,j);
+            }
+        }
+    }
+    return guard_patrol(guard_pos, &matrix, (-1, -1)).0.iter().filter(|block_pos| guard_patrol(guard_pos, &matrix, *block_pos.0).1).count() as u32;
 }
